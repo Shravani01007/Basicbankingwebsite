0afd57a5768218ac6cf9b4994e881677181026ff
Shravani Dhuri <shravanidhuri1020@gmail.com>
2021-02-09 15:34:20 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<link href="1.css" type="text/css" rel="stylesheet">
+		<title>Basic Banking System </title>
+	</head>
+    <body>
+		<div class="h">
+			<div class="ih nav nav-bar">
+				<nav>
+					<ul>
+						<li><a href="index.php" class="logo">Basic Banking System By Shravani Dhuri</a></li>
+						<li><a href="his.php" class="link">Transaction History</a></li>
+						<li><a href="users.php" class="link">Users</a></li>
+						<li><a href="index.php" class="link">Home</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+  
+		<section id="home">
+			<div class="container">
+			</div>
+		</section>	
+		
+		<section id="cus">
+		  <center style="color:rgb(79, 118, 202)"><h1><b><u>Basic Banking System</u></b></h1>
+		  <h3>By Shravani Dhuri</h3><br>
+		  </center>
+		 </section> 
+		<div class="container">
+            <div class="row text-center" id="item_list">
+                <div class="col-sm-6">
+                    <a href="users.php" >
+                        <div class="thumbnail">
+                            <img src="customer2.png" alt="">
+							<div class="caption">
+                                <h1>Users Details</h1>
+                                
+                            </div>
+                        </div> 
+                    </a>
+                </div>
+				<div class="col-sm-6">
+                    <a href="his.php" >
+                        <div class="thumbnail">
+                            <img src="transaction.jpg" alt="">
+                            <div class="caption">
+								<h1>Transaction History</h1>
+                                
+                            </div>
+                        </div> 
+                    </a>
+                </div>
+			</div>
+	  </body>
+</html>
